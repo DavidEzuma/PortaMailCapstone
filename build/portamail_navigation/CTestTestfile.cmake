47a736adc6b8819f09081a7e3ce50ab36d9132db
David Ezuma <davidEzuma42@gmail.com>
2026-01-23 00:05:02 -0600

    navigation progress
    navigation package has been developed up till the launch file. I haven't tested if the addition of this launch file leaves it buildable, as the last install environment (which includes its installation) in the CMakeList text file was commented out beforehand.

---
+++ build/portamail_navigation/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/Users/David/OneDrive/Documents/Projects/PortaMailCapstone/src/portamail_navigation
+# Build directory: /mnt/c/Users/David/OneDrive/Documents/Projects/PortaMailCapstone/build/portamail_navigation
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
