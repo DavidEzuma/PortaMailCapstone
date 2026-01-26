383f76325b95bd1a65938f79e518563ea460f5c4
David Ezuma - from Pi <davidezuma42@gmail.com>
2026-01-25 20:34:31 -0600

    confrimation commit that everything is installable and built through setup sh

--- build/portamail_navigation/CTestTestfile.cmake
+++ build/portamail_navigation/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /mnt/c/Users/David/OneDrive/Documents/Projects/PortaMailCapstone/src/portamail_navigation
-# Build directory: /mnt/c/Users/David/OneDrive/Documents/Projects/PortaMailCapstone/build/portamail_navigation
+# Source directory: /home/davidezuma/PortaMailCapstone/src/portamail_navigation
+# Build directory: /home/davidezuma/PortaMailCapstone/build/portamail_navigation
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
